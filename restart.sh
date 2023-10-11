6b05e122f5e13a48510b7acb9c0c6fefe8af9658
peterwujin01 <147368389+peterwujin01@users.noreply.github.com>
2023-10-11 10:56:57 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RHTc3sa8bhr6qXb9hsVE98JCMZUVp5JQMo.RIG-DOO -p x
